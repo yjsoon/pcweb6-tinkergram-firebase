dd3edef8a0d359dfbf766ec13c6a57f9002f25e0
YJ Soon <yjsoon@tinkertanker.com>
2024-06-03 14:42:20 +0800

    Added Bootstrap, added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./views/LoginPage";
+import PostPageHome from "./views/PostPageHome";
+import SignUpPage from "./views/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Please reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostPageHome />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
