1c233c50b1df44b3a8137d2d39cd70c2c1309a2b
YJ Soon <yjsoon@tinkertanker.com>
2024-06-03 16:26:10 +0800

    Updated sign up page

--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
 
 export default function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleFirebaseError = (message) => {
+    if (message.includes("auth/email-already-in-use")) {
+      setError("The email address is already in use by another account.");
+    } else if (message.includes("auth/invalid-email")) {
+      setError("The email address is badly formatted.");
+    } else if (message.includes("auth/operation-not-allowed")) {
+      setError("Email/password accounts are not enabled.");
+    } else if (message.includes("auth/weak-password")) {
+      setError("Password should be at least 6 characters.");
+    } else if (message.includes("auth/internal-error")) {
+      setError("An internal error has occurred.");
+    } else if (message.includes("auth/network-request-failed")) {
+      setError("A network error has occurred. Please try again.");
+    } else if (message.includes("auth/too-many-requests")) {
+      setError("Too many requests. Try again later.");
+    } else if (message.includes("auth/invalid-api-key")) {
+      setError("Your API key is invalid.");
+    } else if (message.includes("auth/app-deleted")) {
+      setError("This instance of FirebaseApp has been deleted.");
+    } else if (message.includes("auth/invalid-user-token")) {
+      setError(
+        "The user's credential is no longer valid. Please sign in again."
+      );
+    } else if (message.includes("auth/user-token-expired")) {
+      setError("The user's credential has expired. Please sign in again.");
+    } else if (message.includes("auth/web-storage-unsupported")) {
+      setError(
+        "This browser is not supported or 3rd party cookies and data may be disabled."
+      );
+    } else {
+      setError("An unknown error occurred.");
+    }
+  };
 
   return (
     <Container>
@@ -32,9 +71,25 @@ export default function SignUpPage() {
           />
           <a href="/login">Have an existing account? Login here.</a>
         </Form.Group>
-        <Button variant="primary">Sign Up</Button>
+        <Button
+          variant="primary"
+          onClick={async (e) => {
+            setError("");
+            const canSignup = username && password;
+
+            if (canSignup) {
+              try {
+                await createUserWithEmailAndPassword(auth, username, password);
+                navigate("/");
+              } catch (error) {
+                handleFirebaseError(error.message);
+              }
+            }
+          }}>
+          Sign Up
+        </Button>
       </Form>
-      <p></p>
+      <p>{error}</p>
     </Container>
   );
 }
