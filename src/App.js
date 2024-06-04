6ba42dd66d62ca25388f049d999feca90a031b5c
YJ Soon <yjsoon@tinkertanker.com>
2024-06-04 10:44:08 +0800

    Front page now reads from Firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
 import PostPageHome from "./views/PostPageHome";
 import SignUpPage from "./views/SignUpPage";
+import PostPageAdd from "./views/PostPageAdd";
+import PostPageDetails from "./views/PostPageDetails";
+import PostPageUpdate from "./views/PostPageUpdate";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<PostPageHome />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/add" element={<PostPageAdd />} />
+        <Route path="/post/:id" element={<PostPageDetails />} />
+        <Route path="/update/:id" element={<PostPageUpdate />} />
       </Routes>
     </BrowserRouter>
   );
